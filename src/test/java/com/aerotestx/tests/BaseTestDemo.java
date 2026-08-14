2ff8bef5ff0ef5d13eb175fbb2410595866a0b44
dheena <23dheen@gmail.com>
2026-08-14 13:26:04 +0530

    Initial AeroTestX framework setup

---
+++ src/test/java/com/aerotestx/tests/BaseTestDemo.java
@@ -0,0 +1,18 @@
+package com.aerotestx.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.aerotestx.base.BaseTest;
+
+public class BaseTestDemo extends BaseTest{
+
+	@Test
+	public void verifyGoogleTitle() {
+		String Actual = driver.getTitle();
+		
+		System.out.println("Actual Title: " +Actual);
+		Assert.assertEquals(Actual, "Google"); 
+	}
+
+}
